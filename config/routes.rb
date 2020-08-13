0f956a925988b8d5ca030f6b95ab1f420b3344ba
ShotaMasuo <63441901+ShotaMasuo@users.noreply.github.com>
2020-08-13 11:56:13 +0900

    createShowCardFunc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :credit_cards, only: [:new, :create]
+  resources :credit_cards, only: [:new, :create, :show, :destroy]
   
   devise_for :users, controllers: {
     registrations: 'users/registrations'
