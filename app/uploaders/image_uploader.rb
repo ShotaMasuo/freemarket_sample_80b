3d11630dbd158d7d7766b76bdbbfc0b335c8bc3a
shun58 <fool_around256@hotmail.com>
2020-08-14 17:44:48 +0900

    S3 再修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -28,10 +28,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  Create different versions of your uploaded files:
-  version :thumb do
+  # Create different versions of your uploaded files:
+  # version :thumb do
     process resize_to_fit: [150, 150]
-  end
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
