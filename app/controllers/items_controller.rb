4e8413d748f4f20ffbb5559d3c6754c5b0b539f3
mikihisa0529 <67867787+mikihisa0529@users.noreply.github.com>
2020-08-19 17:09:48 +0900

    products_category

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   
   before_action :get_categories
   
-  before_action :get_item, except: [:index, :new, :create]
+  before_action :get_item, only: :show
 
   def index
     @items = Item.includes(:images).order('created_at DESC').limit(5)
@@ -14,16 +14,8 @@ class ItemsController < ApplicationController
 
 
   def new
-    @category_array = []
-    @categories.each do |category|
-      category.children.each do |child|
-        child.children.each do |gchild|
-          @category_array << [category.name, category.id]
-          @category_array << [child.name, child.id]
-          @category_array << [gchild.name, gchild.id]
-        end
-      end
-    end
+    @category_array = Category.where(ancestry:nil)
+    
     @item = Item.new
     @item.images.new
   end
@@ -87,6 +79,14 @@ class ItemsController < ApplicationController
         redirect_to root_path
       end
     end
+  end
+
+  def get_category_children
+    @category_children = Category.find(params[:category_id]).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find(params[:child_id]).children
   end
 
   private
